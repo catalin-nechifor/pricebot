2a40d77e7ee578f389b0c3bc088c33553d9ee1cc
Catalin Nechifor <catalin.nechifor@keysight.com>
2026-06-22 12:53:13 +0300

    always send mail

--- pricebot.py
+++ pricebot.py
@@ -102,7 +102,7 @@ def main():
     timestamp = datetime.now().isoformat(timespec="seconds")
 
     report = []
-    send_alert = False
+    send_alert = True
 
     for p in cfg["products"]:
         name = p["name"]
